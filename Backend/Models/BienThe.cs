b319c2b2722bf807e20496f8165267eea4214edd
ledinhthai22 <ledinhthai0222@gmail.com>
2026-01-04 19:05:48 +0700

    update BienThe.cs

--- Backend/Models/BienThe.cs
+++ Backend/Models/BienThe.cs
@@ -7,35 +7,26 @@ namespace Backend.Models
     public class BienThe
     {
         [Key]
-        public int MaBTSP { get; set; } // Mã Biến Thể Sản Phẩm
-
+        public int MaBTSP { get; set; } 
         [Column(TypeName = "decimal(18,2)")]
         public decimal GiaBan { get; set; }
-
         [Column(TypeName = "decimal(18,2)")]
         public decimal GiaKhuyenMai { get; set; }
-
         public string MauSac { get; set; } = null!;
         public string Ram { get; set; } = null!;
         public string OCung { get; set; } = null!;
         public string ManHinh { get; set; } = null!;
         public string BoXuLyDoHoa { get; set; } = null!;
         public string BoXuLyTrungTam { get; set; } = null!;
-
         public int SoLuongTon { get; set; }
         public bool TrangThai { get; set; } = true;
-
-        // FK SanPham
         public int MaSanPham { get; set; }
         [ForeignKey(nameof(MaSanPham))]
         public SanPham SanPham { get; set; } = null!;
-
-        // Collections
         public ICollection<HinhAnhSanPham> HinhAnhSanPham { get; set; } = new List<HinhAnhSanPham>();
         public ICollection<ChiTietGioHang> ChiTietGioHang { get; set; } = new List<ChiTietGioHang>();
         public ICollection<YeuThich> YeuThich { get; set; } = new List<YeuThich>();
-        
-        // Dựa trên việc DonHang có MaBienThe, ta cần relation ở đây
+
         public ICollection<DonHang> DonHang { get; set; } = new List<DonHang>(); 
     }
 }
