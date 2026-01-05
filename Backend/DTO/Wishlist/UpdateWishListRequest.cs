1fe7a97f469776079569add27f6ac00f955f7ed5
wMuoi50 <quockhanhp519@gmail.com>
2026-01-06 00:53:09 +0700

    BE:Add DTO Wishlist

---
+++ Backend/DTO/Wishlist/UpdateWishListRequest.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.DTO.Wishlist
+{
+    public class UpdateWishListRequest : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Backend/DTO/Wishlist/WishlistResult.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.DTO.Wishlist
+{
+    public class Wishlist : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
