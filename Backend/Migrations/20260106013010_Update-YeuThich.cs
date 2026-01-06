313ddaaf8411869d13ed9112eb3b84dbec21ed09
wMuoi50 <quockhanhp519@gmail.com>
2026-01-06 10:19:30 +0700

    Fix Model YeuThich and Run Migration

---
+++ Backend/Migrations/20260106013010_Update-YeuThich.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ecommerce.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateYeuThich : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Deleted_At",
+                table: "YeuThich",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.UpdateData(
+                table: "NguoiDung",
+                keyColumn: "MaNguoiDung",
+                keyValue: 1,
+                columns: new[] { "MatKhauMaHoa", "NgayCapNhat", "NgayTao" },
+                values: new object[] { "$2a$11$PrRp08.PKgq45bi6ItiJKeCB0a/M45aK6gJA8XK7y3WzhlaTEwOL.", new DateTime(2026, 1, 6, 8, 30, 9, 829, DateTimeKind.Local).AddTicks(9546), new DateTime(2026, 1, 6, 8, 30, 9, 829, DateTimeKind.Local).AddTicks(9528) });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Deleted_At",
+                table: "YeuThich",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "NguoiDung",
+                keyColumn: "MaNguoiDung",
+                keyValue: 1,
+                columns: new[] { "MatKhauMaHoa", "NgayCapNhat", "NgayTao" },
+                values: new object[] { "$2a$11$R2lrMeGHBIRESeJYLQ3bcuZBvn.LPIa9jrW66rpJldJagCv6nGqmO", new DateTime(2026, 1, 6, 0, 16, 23, 691, DateTimeKind.Local).AddTicks(9599), new DateTime(2026, 1, 6, 0, 16, 23, 691, DateTimeKind.Local).AddTicks(9584) });
+        }
+    }
+}
--- Backend/Models/YeuThich.cs
+++ Backend/Models/YeuThich.cs
@@ -14,6 +14,6 @@ namespace Ecommerce.Models
         public int MaBienThe { get; set; }
         [ForeignKey(nameof(MaBienThe))]
         public virtual BienThe BienThe { get; set; } = null!;
-        public DateTime Deleted_At { get; set; } = DateTime.Now;
+        public DateTime? Deleted_At { get; set; }
     }
 }
