e06c37b590c72b76eaf3736096742983e570b26c
wMuoi50 <quockhanhp519@gmail.com>
2026-01-07 14:35:41 +0700

    BE: Add DTO-Cart

---
+++ Backend/DTO/Cart/CartRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Backend.DTO.Cart
+{
+    public class CartRequest
+    {
+        public int UserId { get; set; }
+        public int VariantId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
