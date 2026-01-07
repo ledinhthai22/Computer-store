bd5c447e3cc16a8278c6fde1742b94ebdce4a851
wMuoi50 <quockhanhp519@gmail.com>
2026-01-07 15:37:37 +0700

    BE: Fix DTO-Cart

--- Backend/DTO/Cart/CartItemRequest.cs
+++ Backend/DTO/Cart/CartItemRequest.cs
@@ -1,7 +1,10 @@
-﻿namespace Backend.DTO.Cart
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Backend.DTO.Cart
 {
-    public class CartRequest
+    public class CartItemRequest
     {
+        [Required]
         public int UserId { get; set; }
         public int VariantId { get; set; }
         public int Quantity { get; set; }
---
+++ Backend/DTO/Cart/DeleteCartRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Backend.DTO.Cart
+{
+    public class DeleteCartRequest
+    {
+        [Required]
+        public int UserId { get; set; }
+        public int VariantId { get; set; }
+    }
+}
