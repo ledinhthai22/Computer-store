bdc79ac69add4a00a819d2367f4a942149507fed
wMuoi50 <quockhanhp519@gmail.com>
2026-01-08 20:55:10 +0700

    BE: Fix DTO and Service WebInfo

--- Backend/Services/WebInfo/WebInfoService.cs
+++ Backend/Services/WebInfo/WebInfoService.cs
@@ -24,17 +24,17 @@ namespace Backend.Services.WebInfo
             }
             var result = new WebInfoResult
             {
-                WebInfoId = webInfo.MaThongTin,
-                WebName = webInfo.TenTrang,
-                Address = webInfo.DiaChi,
-                Phone = webInfo.SoDienThoai,
-                LinkFacebook = webInfo.DuongDanFacebook,
-                LinkInstagram = webInfo.DuongDanInstagram,
-                LinkYoutube = webInfo.DuongDanYoutube,
-                PrivacyPolicy = webInfo.ChinhSachBaoMat,
-                ReturnPolicy = webInfo.ChinhSachDoiTra,
-                Termsofuse = webInfo.DieuKhoanSuDung,
-                LinkImage = webInfo.DuongDanAn
+                MaThongTinTrang = webInfo.MaThongTin,
+                TenTrang = webInfo.TenTrang,
+                DiaChi = webInfo.DiaChi,
+                SoDienThoai = webInfo.SoDienThoai,
+                DuongDanFacebook = webInfo.DuongDanFacebook,
+                DuongDanInstagram = webInfo.DuongDanInstagram,
+                DuongDanYoutube = webInfo.DuongDanYoutube,
+                ChinhSachBaoMat = webInfo.ChinhSachBaoMat,
+                ChinhSachDoiTra = webInfo.ChinhSachDoiTra,
+                DieuKhoanSuDung = webInfo.DieuKhoanSuDung,
+                DuongDanAnh = webInfo.DuongDanAn
             };
             return result;
         }
@@ -45,17 +45,17 @@ namespace Backend.Services.WebInfo
                 .Where(t => t.IsDelete != null)
                 .Select(t=> new WebInfoResult
                 {
-                    WebInfoId = t.MaThongTin,
-                    WebName = t.TenTrang,
-                    Address = t.DiaChi,
-                    Phone = t.SoDienThoai,
-                    LinkFacebook = t.DuongDanFacebook,
-                    LinkInstagram = t.DuongDanInstagram,
-                    LinkYoutube = t.DuongDanYoutube,
-                    PrivacyPolicy = t.ChinhSachBaoMat,
-                    ReturnPolicy = t.ChinhSachDoiTra,
-                    Termsofuse = t.DieuKhoanSuDung,
-                    LinkImage = t.DuongDanAn
+                    MaThongTinTrang = t.MaThongTin,
+                    TenTrang = t.TenTrang,
+                    DiaChi = t.DiaChi,
+                    SoDienThoai = t.SoDienThoai,
+                    DuongDanFacebook = t.DuongDanFacebook,
+                    DuongDanInstagram = t.DuongDanInstagram,
+                    DuongDanYoutube = t.DuongDanYoutube,
+                    ChinhSachBaoMat = t.ChinhSachBaoMat,
+                    ChinhSachDoiTra = t.ChinhSachDoiTra,
+                    DieuKhoanSuDung = t.DieuKhoanSuDung,
+                    DuongDanAnh = t.DuongDanAn
                 })
                 .ToListAsync();
             if (webInfo == null)
@@ -79,16 +79,16 @@ namespace Backend.Services.WebInfo
         {
             var WebInfo = new ThongTinTrang
             {
-                TenTrang = request.WebName,
-                SoDienThoai = request.Phone,
-                DiaChi = request.Address,
-                DuongDanFacebook = request.LinkFacebook,
-                DuongDanInstagram = request.LinkInstagram,
-                DuongDanYoutube = request.LinkYoutube,
-                ChinhSachBaoMat = request.PrivacyPolicy,
-                ChinhSachDoiTra = request.ReturnPolicy,
-                DieuKhoanSuDung = request.Termsofuse,
-                DuongDanAn = request.LinkImage
+                TenTrang = request.TenTrang,
+                SoDienThoai = request.SoDienThoai,
+                DiaChi = request.DiaChi,
+                DuongDanFacebook = request.DuongDanFacebook,
+                DuongDanInstagram = request.DuongDanInstagram,
+                DuongDanYoutube = request.DuongDanYoutube,
+                ChinhSachBaoMat = request.ChinhSachBaoMat,
+                ChinhSachDoiTra = request.ChinhSachDoiTra,
+                DieuKhoanSuDung = request.DieuKhoanSuDung,
+                DuongDanAn = request.DuongDanAnh
             };
             _DbContext.ThongTinTrang.Add(WebInfo);
             bool result = await _DbContext.SaveChangesAsync() > 0;
@@ -98,48 +98,48 @@ namespace Backend.Services.WebInfo
             }
             return new WebInfoResult
             {
-                WebInfoId = WebInfo.MaThongTin,
-                WebName = WebInfo.TenTrang,
-                Phone = WebInfo.SoDienThoai,
-                Address = WebInfo.DiaChi,
-                LinkFacebook = WebInfo.DuongDanFacebook,
-                LinkInstagram = WebInfo.DuongDanInstagram,
-                LinkYoutube = WebInfo.DuongDanYoutube,
-                PrivacyPolicy = WebInfo.ChinhSachBaoMat,
-                ReturnPolicy = WebInfo.ChinhSachDoiTra,
-                Termsofuse = WebInfo.DieuKhoanSuDung,
-                LinkImage = WebInfo.DuongDanAn
+                MaThongTinTrang = WebInfo.MaThongTin,
+                TenTrang = WebInfo.TenTrang,
+                SoDienThoai = WebInfo.SoDienThoai,
+                DiaChi = WebInfo.DiaChi,
+                DuongDanFacebook = WebInfo.DuongDanFacebook,
+                DuongDanInstagram = WebInfo.DuongDanInstagram,
+                DuongDanYoutube = WebInfo.DuongDanYoutube,
+                ChinhSachBaoMat = WebInfo.ChinhSachBaoMat,
+                ChinhSachDoiTra = WebInfo.ChinhSachDoiTra,
+                DieuKhoanSuDung = WebInfo.DieuKhoanSuDung,
+                DuongDanAnh = WebInfo.DuongDanAn
             };
         }
         public async Task<WebInfoResult> UpdateWebInfo(int id,WebInfoItemRequest request)
         {
             var WebInfo = await _DbContext.ThongTinTrang.FindAsync(id);
             if (WebInfo == null || WebInfo.IsDelete != null) return null;
-            WebInfo.TenTrang = request.WebName;
-            WebInfo.SoDienThoai = request.Phone;
-            WebInfo.DiaChi = request.Address;
-            WebInfo.DuongDanFacebook = request.LinkFacebook;
-            WebInfo.DuongDanInstagram = request.LinkInstagram;
-            WebInfo.DuongDanYoutube = request.LinkYoutube;
-            WebInfo.ChinhSachBaoMat = request.PrivacyPolicy;
-            WebInfo.ChinhSachDoiTra = request.ReturnPolicy;
-            WebInfo.DieuKhoanSuDung = request.Termsofuse;
-            WebInfo.DuongDanAn = request.LinkImage;
+            WebInfo.TenTrang = request.TenTrang;
+            WebInfo.SoDienThoai = request.SoDienThoai;
+            WebInfo.DiaChi = request.DiaChi;
+            WebInfo.DuongDanFacebook = request.DuongDanFacebook;
+            WebInfo.DuongDanInstagram = request.DuongDanInstagram;
+            WebInfo.DuongDanYoutube = request.DuongDanYoutube;
+            WebInfo.ChinhSachBaoMat = request.ChinhSachBaoMat;
+            WebInfo.ChinhSachDoiTra = request.ChinhSachDoiTra;
+            WebInfo.DieuKhoanSuDung = request.DieuKhoanSuDung;
+            WebInfo.DuongDanAn = request.DuongDanAnh;
             bool result = await _DbContext.SaveChangesAsync()>0;
             if (!result) { throw new InvalidOperationException($"Cập nhật thông tin trang không thành công!"); }
             return new WebInfoResult
             {
-                WebInfoId = WebInfo.MaThongTin,
-                WebName = WebInfo.TenTrang,
-                Phone = WebInfo.SoDienThoai,
-                Address = WebInfo.DiaChi,
-                LinkFacebook = WebInfo.DuongDanFacebook,
-                LinkInstagram = WebInfo.DuongDanInstagram,
-                LinkYoutube = WebInfo.DuongDanYoutube,
-                PrivacyPolicy = WebInfo.ChinhSachBaoMat,
-                ReturnPolicy = WebInfo.ChinhSachDoiTra,
-                Termsofuse = WebInfo.DieuKhoanSuDung,
-                LinkImage = WebInfo.DuongDanAn
+                MaThongTinTrang = WebInfo.MaThongTin,
+                TenTrang = WebInfo.TenTrang,
+                SoDienThoai = WebInfo.SoDienThoai,
+                DiaChi = WebInfo.DiaChi,
+                DuongDanFacebook = WebInfo.DuongDanFacebook,
+                DuongDanInstagram = WebInfo.DuongDanInstagram,
+                DuongDanYoutube = WebInfo.DuongDanYoutube,
+                ChinhSachBaoMat = WebInfo.ChinhSachBaoMat,
+                ChinhSachDoiTra = WebInfo.ChinhSachDoiTra,
+                DieuKhoanSuDung = WebInfo.DieuKhoanSuDung,
+                DuongDanAnh = WebInfo.DuongDanAn
             };
         }
         public async Task<bool> RestoreWebInfo(int id)
@@ -150,7 +150,7 @@ namespace Backend.Services.WebInfo
                 return false;
             }
             var WebInfoOnl = await GetAsync();
-            if(WebInfoOnl != null)  await SoftDelete(WebInfoOnl.WebInfoId);
+            if(WebInfoOnl != null)  await SoftDelete(WebInfoOnl.MaThongTinTrang);
             webInfo.IsDelete = null;
             _DbContext.ThongTinTrang.Update(webInfo);
             return await _DbContext.SaveChangesAsync() > 0;
