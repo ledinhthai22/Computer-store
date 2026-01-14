187444667ed5d47d13d88f8a654b560fdd09bb8a
ledinhthai22 <ledinhthai0222@gmail.com>
2026-01-14 19:05:21 +0700

    fix/merge conflict 2

--- Frontend/src/pages/Admin/product/ProductDetail.jsx
+++ Frontend/src/pages/Admin/product/ProductDetail.jsx
@@ -434,6 +434,7 @@ const ProductDetail = () => {
                       {brand.tenThuongHieu}
                     </option>
                   ))}
+                  ///conflict
                 </select>
                 <div className="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
                   <ChevronDown size={18} className={`text-gray-400 transition-transform duration-200 ${!isEditing ? 'opacity-50' : ''}`} />
