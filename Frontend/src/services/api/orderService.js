82c109ee4c45534157685f4695f43cd1e2132641
KhClnG <kgming107@gmail.com>
2026-01-17 23:15:49 +0700

    order-service

---
+++ Frontend/src/services/api/orderService.js
@@ -0,0 +1,59 @@
+import axiosClient from "./axiosClient";
+
+export const orderService = {
+  // USER
+
+  // ADMIN
+  getAdminList: async () => {
+    const res = await axiosClient.get("/admin/order");
+    return res.data;
+  },
+
+  getByStatus: async (status) => {
+    const res = await axiosClient.get(`/admin/order/status/${status}`);
+    return res.data;
+  },
+
+  getByMaDonHang: async (maDonHang) => {
+    const res = await axiosClient.get(`/admin/order/Ma-Don-Hang/${maDonHang}`);
+    return res.data;
+  },
+
+  getByMaDon: async (maDon) => {
+    const res = await axiosClient.get(`/admin/order/Ma-Don/${maDon}`);
+    return res.data;
+  },
+
+  getBySoDienThoat: async (soDienThoat) => {
+    const res = await axiosClient.get(
+      `/admin/order/So-Dien-Thoai/${soDienThoat}`,
+    );
+    return res.data;
+  },
+
+  updateStatus: async (maDonHang, status) => {
+    const res = await axiosClient.put(`/admin/products/${maDonHang}`, status);
+    return res.data;
+  },
+
+  updateInfo: async (maDonHang, data) => {
+    const res = await axiosClient.put(
+      `/admin/products/Admin-Cap-nhat-thong-tin/${maDonHang}`,
+      data,
+    );
+    return res.data;
+  },
+
+  create: async (data) => {
+    const res = await axiosClient.post(`/admin/order/Tao-Don-Hang`, data);
+    return res.data;
+  },
+};
+
+// Xử lý bug
+export const handleApiError = (error, defaultMessage = "Có lỗi xảy ra") => {
+  const message =
+    error.response?.data?.message || error.message || defaultMessage;
+  console.error("API Error:", error);
+  return message;
+};
