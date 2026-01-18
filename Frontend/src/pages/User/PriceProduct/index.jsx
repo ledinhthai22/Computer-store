07c43fecb6fc68f15b0df9b1f7faf6ce29a5d0cf
Phamkhactuyen <phamtuyen27042005@gmail.com>
2026-01-18 19:52:36 +0700

    fix layout user

--- Frontend/src/pages/User/PriceProduct/index.jsx
+++ Frontend/src/pages/User/PriceProduct/index.jsx
@@ -129,7 +129,7 @@ export default function PriceProduct() {
     // --- 5. Render ---
     return (
         <div className="w-full bg-stone-100 py-8 min-h-screen">
-            <div className="container mx-auto px-4">
+            <div className="container mx-auto max-w-7xl px-3 sm:px-4 lg:px-8 pt-4 pb-8 lg:pt-8 lg:pb-12 flex flex-col gap-4 lg:gap-8">
                 
                 <div className="mb-6 flex justify-between items-center">
                     <h2 className="text-2xl font-bold text-gray-800 border-l-4 border-[#2f9ea0] pl-3">
